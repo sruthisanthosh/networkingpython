f6cc3dffcb50b13f25d4e9ab8b2de3aff5392e4a
Sruthi Santhosh <srutisantosh@gmail.com>
2025-07-23 02:54:52 +1000

    strip lines from a file

--- network_ping_tool/ping_ips.py
+++ network_ping_tool/ping_ips.py
@@ -1,7 +1,9 @@
 
 
-with open('ip_list.txt') as file:
-    ip=file.strip();
+with open('/Users/sruthisanthosh/Documents/networkingpython/networkingpython/network_ping_tool/ip_list.txt') as file:
+    ip_list= [ line.strip() for line in file ];
 
-print(ip);
+print(ip_list);
 
+#Output:
+#['8.8.8.8', '1.1.1.1', '192.0.2.1']
