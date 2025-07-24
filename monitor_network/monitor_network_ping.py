db74a860329c61c6703460c673042db4f756da49
Sruthi Santhosh <srutisantosh@gmail.com>
2025-07-24 15:52:48 +1000

    added code to supress output and sleep

--- monitor_network/monitor_network_ping.py
+++ monitor_network/monitor_network_ping.py
@@ -1,5 +1,6 @@
 import subprocess;
 import datetime;
+import time;
 
 with open('monitor_network/ip_list.txt') as file:
     ip_list = [ line.strip() for line in file ];
@@ -9,8 +10,7 @@ with open('monitor_network/monitor_logs','w') as log:
     
     while(datetime.datetime.now() < end_time):
         for ip in ip_list:
-            result = subprocess.run(['ping','-c','1',ip]);
-            
+            result = subprocess.run(['ping','-c','1',ip],stdout=subprocess.DEVNULL, stderr = subprocess.DEVNULL);
             log.write(f"{datetime.datetime.now()} Status of {ip} is {result.returncode} \n");
+            time.sleep(5);
                   
- 
