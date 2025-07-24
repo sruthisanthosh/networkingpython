72387be8e52e79fda5f8ffa2540fbd2740259ced
Sruthi Santhosh <srutisantosh@gmail.com>
2025-07-24 18:12:09 +1000

    adding try and exception blocks

--- ssh_monitor/ssh_network.py
+++ ssh_monitor/ssh_network.py
@@ -9,13 +9,18 @@ password=config.password;
 client = paramiko.SSHClient();
 client.load_system_host_keys();
 client.set_missing_host_key_policy(paramiko.AutoAddPolicy());
-client.connect(ip,22,user,password);
-
-stdin, stdout, stderr = client.exec_command('uptime');
-
 with open('monitor_device_logs.txt','w') as log:
-    log.write(f"{datetime.datetime.now()} {ip} {stdout.read().decode()} {stderr.read().decode()}");
+    try:
+        client.connect(ip,22,user,password);
+
+        stdin, stdout, stderr = client.exec_command('uptime');
+
+        
+        log.write(f"{datetime.datetime.now()} STDOUT: {ip} {stdout.read().decode()}");
+        log.write(f"{datetime.datetime.now()} STDERR: {ip} {stderr.read().decode()}");
+
+
+    except Exception as e:
+        log.write(f"Exception occured while connecting to the ssh server: {e}");
 
 client.close();
-
-
