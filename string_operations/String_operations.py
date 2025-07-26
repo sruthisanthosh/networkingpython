35ab71252a8a0b59528eadc64813936a26cf94b9
Sruthi Santhosh <srutisantosh@gmail.com>
2025-07-26 16:13:02 +1000

    adding output log file

--- string_operations/String_operations.py
+++ string_operations/String_operations.py
@@ -1,47 +1,50 @@
 import re
 #String Operations: Strip, Split, Slice, Regex 
  
-#strip
-line = "   Interface Gi0/1 is up     \n"
-print(line.strip())
+with open('string_operations/output_log.txt','w') as log :
 
-print("*"*10)
+    #strip
+    line = "   Interface Gi0/1 is up     \n"
+    log.write(f'{line.strip()}\n')
 
-#split
-output = "Filesystem      Size  Used Avail Use% Mounted on\n/dev/sda1        50G   25G   25G  50% /"
-lines = output.split('\n')
-headers = lines[0].split()
-values = lines[1].split()
-print(f"Mount point: {values[-1]}, Usage: {values[-2]}")
+    log.write(f"{'*'*10}\n")
 
-print("*"*10)
+    #split
+    output = "Filesystem      Size  Used Avail Use% Mounted on\n/dev/sda1        50G   25G   25G  50% /"
+    lines = output.split('\n')
+    headers = lines[0].split()
+    values = lines[1].split()
+    log.write(f"Mount point: {values[-1]}, Usage: {values[-2]}\n")
 
-#split 2
-line = " 18:09:36 up 5 days,  3:42,  2 users,  load average: 0.08, 0.09, 0.08"
-parts = line.split(" up ")
-print(parts)
-uptime_section = parts[1].split(",")[0]  
-print(uptime_section)
+    log.write(f"{'*'*10}\n")
 
-print("*"*10)
+    #split 2
+    line = " 18:09:36 up 5 days,  3:42,  2 users,  load average: 0.08, 0.09, 0.08"
+    parts = line.split(" up ")
+    uptime_section = parts[1].split(",")[0]  
+    log.write(f"{uptime_section}\n")
 
-#slicing
-interface = "GigabitEthernet0/1"
-print(interface[0:15])
-print(interface[15:18])
+    log.write(f"{'*'*10}\n")
 
-print("*"*10)
+    #slicing
+    interface = "GigabitEthernet0/1"
+    log.write(f"{interface[0:15]}\n")
+    log.write(f"{interface[15:18]}\n")
 
-#regex 1
-line = "CPU usage: user 12.5%, system 7.3%, idle 80.2%"
-pattern=r'\d+\.\d+'
-print(re.findall(pattern,line))
+    log.write(f"{'*'*10}\n")
 
-print("*"*10)
+    #regex 1
+    line = "CPU usage: user 12.5%, system 7.3%, idle 80.2%"
+    pattern=r'\d+\.\d+'
+    result=re.findall(pattern,line)
+    log.write(f"{str(result)}\n")
 
-#regex 2
-log_line = "Device connected with IP: 192.168.10.254 on port 22"
-pattern=r'\d+\.\d+\.\d+\.\d+'
-print(re.findall(pattern,log_line))
+    log.write(f"{'*'*10}\n")
 
-print("*"*10)
+    #regex 2
+    log_line = "Device connected with IP: 192.168.10.254 on port 22"
+    pattern=r'\d+\.\d+\.\d+\.\d+'
+    result=re.findall(pattern,log_line)
+    log.write(f"{str(result)}\n")
+
+    log.write(f"{'*'*10}\n")
